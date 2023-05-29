4645fcb49e6b7128329528274108b2c8e7a68091
Dainius <dainiusnarkevicius1@gmail.com>
2023-05-29 15:06:22 +0300

    update

--- js/01-console.js
+++ js/01-console.js
@@ -1,3 +1,7 @@
 console.clear();
 
 console.log(4, 5, 6, 7);
+
+// single line comment
+
+/*  multi lines comment */
---
+++ js/02-numbers.js
@@ -0,0 +1,50 @@
+console.clear();
+
+
+/*  Sveikieji/desimtainiai
+         Teigiami/neigiami
+         Mistiniai: begalybes ir NaN (not a number)
+
+         Aritmetiniai operatoriai: 
+         +, -, *, / 
+
+         Salia matematikos: 
+         (, )
+
+         Kintamuju inicijavimo budai: 
+         const - default
+         let - naudoti tik jei reikia keisti reiksmes
+         var - senove, todel niekada nenaudoti, nebent TIKRAI zinai ka darai
+*/
+
+
+console.log(1);
+console.log(3.14);
+console.log(-3);
+
+console.log(Infinity);
+console.log(-Infinity);
+
+console.log(NaN); 
+
+console.log(2 + 2); 
+console.log(2 + 2 * 2);
+console.log(2 + 2 * 2, 8, 10);
+
+
+console.clear();
+
+// Kintamieji
+
+const amzius = 20;
+
+
+console.clear();
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = 5;
+console.log(pinigine);
+
+// Kintamieji: konstantos ir kintamieji
