40e68a721dec596d012c9a4f6874213a56a6cd78
Dainius <dainiusnarkevicius1@gmail.com>
2023-05-30 13:44:12 +0300

    strings update

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -6,7 +6,10 @@ console.clear();
          Mistiniai: begalybes ir NaN (not a number)
 
          Aritmetiniai operatoriai: 
-         +, -, *, / 
+         +, -, *, /, ** (kelimas laipsniu), 
+
+         Priskirimo operatoriai: 
+         =, +=, -=, *=, /=, **=, ++, --
 
          Salia matematikos: 
          (, )
@@ -48,3 +51,28 @@ pinigine = 5;
 console.log(pinigine);
 
 // Kintamieji: konstantos ir kintamieji
+
+console.clear();
+
+console.log(m);   // 7  //turiu 7
+consoloe.log(++m); // 8   // gaunu 9 -> log
+console.log(m++); // 8 //log 8 -> 9
+console.log(m++); // 9   // 9 -> 10
+console.log(++m); //11   // 11 - > log.
+
+
+console.clear();
+// console.log(81 ** 0.5); // taip galima istraukt kvadratine sakni.//
+
+// console.log(16 ** 0.25) // 4 laipsnio saknis// 
+
+// console.clear();
+
+// const p1 = 10;
+// const p2 = 2;
+// const p3 = 8;
+// const p4 = 4;
+// const p5 = 6;
+
+// const vidurkis = (p1 + p2 + p3 + p4 + p5) / 5;
+// console.log(vidurkis);
---
+++ js/03-strings.js
@@ -0,0 +1,23 @@
+console.clear();
+
+/* string - tekstiniu simboliu grandinele, aka tekstas
+
+Iniciavimo budai: 
+ - viengubos kabutes
+ - dvigubos kabutes
+ */
+
+ const vardas = 'Jonas';
+ const vardas2 = "Maryte";
+
+ console.log(vardas);
+ console.log(vardas2);
+
+ const firstName = 'Ona';
+ const lastName = 'Onaityte';
+
+ const fullName = firstName + ' ' + lastName;   // ' ' duoda tarpa. 
+ console.log(fullName);
+
+ const rekiam = 'Sveiki, as esu ' + firstName + '!';
+ console.log(rekiam)
