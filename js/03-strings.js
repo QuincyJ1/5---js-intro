914a2901a6b0998ebe0b599acaa09866c35b88ce
Dainius <dainiusnarkevicius1@gmail.com>
2023-05-31 10:00:52 +0300

    uzduotis update

--- js/03-strings.js
+++ js/03-strings.js
@@ -21,3 +21,100 @@ Iniciavimo budai:
 
  const rekiam = 'Sveiki, as esu ' + firstName + '!';
  console.log(rekiam)
+
+ const kabute12 = "Vienguba (') ir dvi" + 'dviguba (") kabutes';
+ console.log (kabute12);
+
+ const kabute12_1 = "Vienguba (\') ir dvi" + 'dviguba (\") kabutes';
+ console.log(kabute12_1);   // nesvarbu kaip inicijuosi stringa, jei turinyje surandi kabute, pries visas sudedi backslasha ir escapini nuo klaidu // 
+
+//  const back = 'Backslash (\\)';     
+//  console.log(back);            // reikia \\ kad isspausdintu, nes jei bus \, nespausdins nes pirmiausia ji priims kaip ignoravimo. Tada antra nupies. Tada trecias jei bus (\\\), vel nerodys, bet jei bus ketvirtas (\\\\), rodys \\.
+console.clear();
+ const back = 'Backslash (\\\\)';
+ console.log(back);
+
+
+ console.clear();
+
+//  const labas1 = 'Labas\n\
+//  rytas\n\                             // \n - new line simbolis // 
+//  Lietuva!'; 
+
+
+ const labas2 = `Labas 
+rytas,
+Lietuva!`;
+                console.log(labas2);
+
+
+
+
+
+// stringuose geriausia naudot `` (backtick). taip pat ir formatuoja backtickai automatiskai // 
+
+
+const name = 'Martynas;'
+const age = 99;
+const iLike = 'masina'
+
+const martynas = `Sveiki as esu ${name}, man yra ${age} ir as megstu ${iLike} `;
+console.log(martynas);
+
+
+//${} --- naudojama tik naudojant `` . Taip panaudojami stringai. //;
+
+
+console.clear()
+
+
+// const skaicius = `4`;
+// let daugiklis = `1`;
+// let rezultatas = `4`;
+// let eilute = `${skaicius} * ${daugiklis} = ${rezultatas}`;
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas} `);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*2}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*3}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*4}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*5}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*6}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*7}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*8}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*9}`);
+// console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*10}`);
+
+console.clear();
+
+const skaicius = `9`;
+let daugiklis = `1`;
+let rezultatas = `9`;
+
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*2}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*3}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*4}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*5}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*6}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*7}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*8}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*9}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*10}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/uzduotis.js
@@ -0,0 +1,20 @@
+
+
+const skaicius = `2`;
+let daugiklis = `1`;
+let rezultatas = `2`;
+
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*2}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*3}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*4}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*5}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*6}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*7}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*8}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*9}`);
+console.log(`${skaicius} * ${daugiklis++} = ${rezultatas*10}`);
+
+
+
+
