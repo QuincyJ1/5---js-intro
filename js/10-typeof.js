0f13a89a6e2865bbf78276d028371de1038de7c1
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-07 15:51:00 +0300

    multiply

---
+++ js/10-typeof.js
@@ -0,0 +1,16 @@
+console.clear();
+
+/* typeof - nusako duomens tipa 
+*/
+
+console.log(typeof 5);
+function sum(a,b) {
+    return a + b;
+}
+
+console.log(typeof true)
+console.log(typeof sum);
+console.log(typeof [1,2]);
+
+
+// typeof rezultatas yra stringas. 
---
+++ mini/multiply.js
@@ -0,0 +1,14 @@
+console.clear();
+
+function multiply(a, b) {
+if(typeof a !== 'number') {
+    return 'Error';
+}
+const rez = a * b;
+return rez;
+}
+
+console.log(multiply( 'labas'))
+
+console.log(multiply(undefined, 7));
+console.log(multiply('hello', 7));
