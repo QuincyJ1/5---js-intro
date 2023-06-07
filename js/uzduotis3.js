42d07e6ed7d10b36c20d3af15e31a441c5b48cf3
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-07 13:50:34 +0300

    update

--- js/uzduotis3.js
+++ js/uzduotis3.js
@@ -1,9 +1,12 @@
 console.clear();
 
+
+
 let sum1 = 0
 for (let x = 0; x = 0; x += 0) {
-    console.log(sum1+=x);
+    sum1+=x;
 }
+console.log(sum1)
 
 console.log('---------------------------------')
 
@@ -12,9 +15,9 @@ const end2 = 4;
 let sum2 = 0;
 
 for (let x = start2; x <= end2; x++) {
- console.log(sum2+=x)
+ sum2+=x
 }
-
+console.log(sum2);
 console.log('---------------------------------')
 
 const start3 = 0;
@@ -22,9 +25,9 @@ const end3 = 100;
 let sum3 = 0;
 
 for (let x = start3; x <= end3; x++) {
-    console.log(sum3+=x);
+    sum3+=x
 }
-
+console.log(sum3);
 console.log('-------------------------------------')
 
 const start4 = 574;
@@ -32,9 +35,9 @@ const end4 = 815;
 const step4 = 1;
 let sum4 = 0;
 for (let x = start4; x <= end4; x += step4) {
-    console.log(sum4+=x);
+    sum4+=x;
 }
-
+console.log(sum4);
 console.log ('---------------------------------')
 
 const start5 = -50;
@@ -42,9 +45,9 @@ const end5 = 50;
 let sum5 = 0;
 
 for (let x = start5; x <= end5; x++) {
-    console.log(sum5+=x)
+    sum5+=x;
 }
-
+console.log(sum5);
 console.log('--------------------------------')
 
 
@@ -53,9 +56,9 @@ const end6 = 30;
 let sum6 = 0;
 
 for (let x = start6; x <= end6; x++) {
-    console.log(sum6+=x)
+    sum6+=x
 }
-
+console.log(sum6);
 console.log('2 uzduotis--------------------------------------');
 
 const word = 'abcdef';
@@ -223,6 +226,13 @@ console.log(`m4 ketvertai = ${ketvertai4}`);
 console.log(`m4 penketai = ${penketai4}`)
 
 
+console.clear()
 
+function sumaIntervale(nuo, iki) {
+    let suma = 0
+    for (let i = 0; i < 5; i++) {
 
+    }
+    return suma; 
+}
 
