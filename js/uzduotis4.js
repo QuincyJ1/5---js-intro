f1532b5a5fe8469cc4545433d1cc57c647fbe16d
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-09 14:55:54 +0300

    update

--- js/uzduotis4.js
+++ js/uzduotis4.js
@@ -33,7 +33,7 @@ console.log(isrinktiRaides(1561, 2))
 console.log(isrinktiRaides('', 0))
 console.log(isrinktiRaides( 'abc', 'd'))
 console.log(isrinktiRaides( 'abc', '1'))
-console.log(isrinktiRaides('abc', 0))
+console.log(isrinktiRaides('abcgf', 0))
 console.log(isrinktiRaides('abc', -5))
 console.log(isrinktiRaides( 'abc', 4))
 console.log(isrinktiRaides( 'ab', 43))
@@ -44,10 +44,40 @@ console.log(isrinktiRaides('aaa', null))
 console.log(isrinktiRaides('abcdefg', 2 ))
 console.log(isrinktiRaides('abcdefghijkl', 3))
 
-console.log('-------------------------------')
+
+console.clear();
+console.log('--------------------------------------')
+
+function dalyba (num1, num2) {
+    if (num1 === 0 || num2 === 0) {
+        return 'Nulis cia nera variantas'
+    }
+    
+    if (typeof num1 !== 'number' || !isFinite(num1)) {
+return 'Pirma reiksme turi buti skaicius.'
+    }
+
+    if (typeof num2 !== 'number' || !isFinite(num2)) {
+        return 'Antra reiksme turi buti skaicius.'
+            }
+
+const result = num1 / num2
+    return result;
+}
 
 
-console.log(isrinktiRaides( 'abc', 4))
 
-console.log(isrinktiRaides( 'abcddd', 5))
+console.log(dalyba(5, 2))
+console.log(dalyba(-5, 2))
+console.log(dalyba(Infinity, 2))
+console.log(dalyba(-Infinity, 2))
+console.log(dalyba(0, 2))
+console.log(dalyba(4, 0))
+console.log(dalyba(null, 5))
+console.log(dalyba(5, null))
+console.log(dalyba(NaN, 8))
+console.log(dalyba(4, NaN))
+console.log(dalyba(undefined,6))
+console.log(dalyba(6, undefined))
+console.log('---------------------')
 
