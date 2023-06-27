8e6790765f92e6a1e28ebe9a96183cc184395568
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-27 11:13:16 +0300

    js class

---
+++ js/17-js-class.js
@@ -0,0 +1,16 @@
+console.clear();
+
+import Student from './Student.js';
+
+
+const petras = new Student ('Petras', 2000);
+const maryte = new Student ('Maryte', 1980);//  sukuriami objektai. 
+
+
+console.log(petras.name, petras.marks);
+console.log(maryte.name,  maryte.marks);
+
+
+
+
+
---
+++ js/Student.js
@@ -0,0 +1,12 @@
+console.clear();
+
+class Student {           
+    constructor (vardas, gimimoMetai) {
+        this.name = vardas;
+       this.birthYear = gimimoMetai;
+       this.isMarried = false;
+       this.marks = [];
+    }             
+}
+
+export default Student;
