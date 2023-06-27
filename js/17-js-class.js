8251e964966a22332f2cb7a150dada895f2535af
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-27 16:14:24 +0300

    Student marks average

--- js/17-js-class.js
+++ js/17-js-class.js
@@ -1,14 +1,23 @@
 console.clear();
 
-import Student from './Student.js';
+import { Student } from './Student.js';
 
 
 const petras = new Student ('Petras', 2000);
 const maryte = new Student ('Maryte', 1980);//  sukuriami objektai. 
 
+petras.addMark(8);
+maryte.addMark(9);
+petras.addMark(8);
+maryte.addMark(-2);
+petras.addMark(6);
+maryte.addMark(9);
+petras.addMark(8);
+maryte.addMark(9);
 
-console.log(petras.name, petras.marks);
-console.log(maryte.name,  maryte.marks);
+console.log(petras.name, petras.marks, petras.marksAverage());
+
+console.log(maryte.name,  maryte.marks, maryte.marksAverage());
 
 
 
--- js/Student.js
+++ js/Student.js
@@ -6,7 +6,22 @@ class Student {
        this.birthYear = gimimoMetai;
        this.isMarried = false;
        this.marks = [];
-    }             
+    }      
+    
+    addMark(mark) {                 //metodas itraukiantis nauja pazymi.
+        console.log('bandau prideti pazymi', mark);
+        this.marks.push(mark)
+    }
+
+    marksAverage () {
+        if (this.marks.length === 0) {
+            return 'neiskaita';
+        }
+        const correctMarks = this.marks.filter(m => m > 0);
+
+        console.log('>>>', correctMarks);
+        return this.marks.reduce((t, m) => t + m, 0) / this.marks.length;
+    }
 }
 
-export default Student;
+export { Student }
